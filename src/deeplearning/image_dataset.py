52a5b221ac3728e5ce30b75ae48a9471de9f7b29
Angel Campoverde <angel.campoverde@cern.ch>
2025-04-30 14:13:06 +0800

    Placing file in correct directory

--- src/deeplearning/image_dataset.py
+++ src/deeplearning/image_dataset.py
@@ -6,21 +6,23 @@ https://pytorch.org/tutorials/beginner/basics/data_tutorial.html#creating-a-cust
 
 import os
 import pandas as pd
-from torchvision.io import read_image
+from torchvision.io     import read_image
+from torch.utils.data   import Dataset
 
 class ImageDataset(Dataset):
     '''
     Class meant to implement custom functionality for image processing
     '''
+    # --------------------------------
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
-
+        self._img_labels       = pd.read_csv(annotations_file)
+        self._img_dir          = img_dir
+        self._transform        = transform
+        self._target_transform = target_transform
+    # --------------------------------
     def __len__(self):
-        return len(self.img_labels)
-
+        return len(self._img_labels)
+    # --------------------------------
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
         image = read_image(img_path)
